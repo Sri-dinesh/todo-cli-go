ffb1284e6b547741059b16584ea7385b66235512
Sri-Dinesh <santhisridinesh@gmail.com>
2025-12-10 12:41:49 +0530

    feat: add initial implementation of Todo-CLI with Todo struct and basic methods

---
+++ todo.go
@@ -0,0 +1,51 @@
+package main
+
+import "time"
+
+type Todo struct {
+	Title string
+	Completed bool
+	CreatedAt time.Time
+	CompletedAt *time.Time
+}
+
+
+type Todos []Todo
+
+
+func (todos *Todos) add(title string) {
+	todo := Todo {
+		Title: title,
+		Completed: false,
+		CompletedAt: nil,
+		CreatedAt: time.Now(),
+	}
+
+
+
+	*todos = append(*todos, todo)
+}
+
+func (todos *Todos) validateIndex(index int) error {
+	if index < 0 || index >= len(*todos) {
+		err := erros.New("Invalid index")
+		fmt.println(err)
+		return err
+	}
+
+	return nil
+}
+
+
+func (todos *Todos) delete(index int) error {
+	t := *todos
+
+	if err := todos.validateIndex(index); err != nil {
+		return err
+	}
+
+	*todos = append(t[: index], t[index+1:]...)
+
+	return nil
+	
+}
