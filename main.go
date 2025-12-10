a5bf3708ec3a71ecc336b8a430a624395bfb7fc2
Sri-Dinesh <santhisridinesh@gmail.com>
2025-12-10 13:08:21 +0530

    feat: implement storage functionality and add initial todos data

--- main.go
+++ main.go
@@ -3,17 +3,19 @@ package main
 func main() {
 	todos := Todos{}
 
+	storage := NewStorage[Todos]("todos.json")
+	storage.Load(&todos)
+
 	todos.add("Complete the project")
-	todos.add("Review code")
+	// todos.add("Review code")
 	todos.add("Write documentation")
 
+	todos.toggle(1)
 
-    todos.toggle(1)
+	todos.print()
 
-    todos.print()
-    
-    
+	storage.save(todos)
 
+	// todos.validateIndex(2)
 
-    todos.validateIndex(2)
 }
