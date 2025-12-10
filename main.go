109e2e472f737a3017545d20fea88ec0dba8a313
Sri-Dinesh <santhisridinesh@gmail.com>
2025-12-10 12:59:12 +0530

    feat: initialized Todo-CLI with module and dependencies, implement core functionalities for managing todos - Table Format Render.

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
-
 func main() {
-    fmt.Println("Inside the Todo-CLI-App!")
+	todos := Todos{}
+
+	todos.add("Complete the project")
+	todos.add("Review code")
+	todos.add("Write documentation")
+
+
+    todos.toggle(1)
+
+    todos.print()
+    
+    
+
+
+    todos.validateIndex(2)
 }
